e78f8d1c96fdfb0926296a6b74666493c7d5a680
Izabella Melo <imcm@cin.ufpe.br>
2017-11-08 19:17:54 -0200

    Update dijkstra.cpp

--- graph/dijkstra.cpp
+++ graph/dijkstra.cpp
@@ -13,22 +13,23 @@ priority_queue<ii, vector<ii>, greater<ii> > min_heap;
 
 void dijkstra(int v){
 	D[v] = 0;
-	min_heap.push(ii(0, v));
+	min_heap.push(ii(0, v)); // put initial node in min heap
 
 	while(!min_heap.empty()){
 		ii now = min_heap.top();
 		min_heap.pop();
 
 		visit[now.second] = 1;
-
+		
+		// see all neighbors nodes
 		for(int i = 0; i < graph[now.second].size(); i++){
 			ii next = graph[now.second][i];
 
-			if(visit[next.second] == 0){
-				if(D[next.second] > D[now.second] + next.first){
+			if(visit[next.second] == 0){ // unvisited node
+				if(D[next.second] > D[now.second] + next.first){ // if new distance is smaller than old
 					D[next.second] = D[now.second] + next.first;
 				
-					min_heap.push(ii(D[next.second], next.second));
+					min_heap.push(ii(D[next.second], next.second)); // put new node in min heap
 				}
 			}
 		}
@@ -37,7 +38,7 @@ void dijkstra(int v){
 
 
 int main(){
-  for(int i = 0; i < SizeNodes; i++)
+  for(int i = 0; i < SizeNodes; i++) // init distance
   	D[i] = INF;
 
   //dijkstra(initialNode);
