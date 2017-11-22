fdc23819744b92855ac7d2251aa727ec419e7962
Izabella Melo <imcm@cin.ufpe.br>
2017-11-22 16:05:45 -0200

    Update bfs.cpp

--- graph/bfs.cpp
+++ graph/bfs.cpp
@@ -1,6 +1,9 @@
 /*
     Algoritmo que realiza busca exaustiva num grafo, começando por um vértice inicial v e olhando
     para todos os vértices adjacentes a v até que todos os vértices tenham sido visitados
+    
+    Izabella Melo ~imcm
+    imcm@cin.ufpe.br
 */
 #include <bits/stdc++.h>
 using namespace std;
