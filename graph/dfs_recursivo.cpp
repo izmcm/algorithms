24fb8cc64fd8198960d18001c2705c44ab678572
Izabella Melo <imcm@cin.ufpe.br>
2017-12-01 18:50:49 -0200

    Update dfs_recursivo.cpp

--- graph/dfs_recursivo.cpp
+++ graph/dfs_recursivo.cpp
@@ -1,3 +1,11 @@
+/*
+    Algoritmo que realiza busca exaustiva num grafo, começando por um vértice inicial v e visitando
+    o máximo possivel seu ramo até retroceder para visitar outro ramo até que todos os vértices tenham
+    sido visitados
+    
+    Izabella Melo ~imcm
+    imcm@cin.ufpe.br
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
